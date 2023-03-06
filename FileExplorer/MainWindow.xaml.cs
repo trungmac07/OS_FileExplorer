9ba7e2a75b913a947a6390bd4fd81c169cee4840
caokhoi2702 <ngcaokhoi2702@gmail.com>
2023-03-06 20:12:48 +0700

    thêm cây folder

--- FileExplorer/MainWindow.xaml.cs
+++ FileExplorer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,10 +21,50 @@ namespace FileExplorer
     /// </summary>
     public partial class MainWindow : Window
     {
+        public void getDirectoryTree()
+        {
+            DriveInfo curDrive = DriveInfo.GetDrives()[0];
+            DirectoryInfo curDirectory = curDrive.RootDirectory;
+            DirectoryInfo[] directoryList = curDirectory.GetDirectories();
+
+            foreach (DirectoryInfo directory in directoryList)
+            {
+                //<Button Height="80" Background="#222831" Foreground="#EEEEEE" BorderThickness="0 0 0 0">O DIA C</Button>
+                Button button = new Button();
+                button.Height = 50;
+                button.Background = new SolidColorBrush(Color.FromRgb(57, 62, 70));
+                button.Foreground = new SolidColorBrush(Color.FromRgb(238, 238, 238));
+                button.BorderThickness = new Thickness(0, 0, 0, 0);
+                button.Content = directory.Name;
+                button.HorizontalContentAlignment = HorizontalAlignment.Left;
+                button.Cursor = Cursors.Hand;
+
+                DockPanel dockPanel = new DockPanel();
+                TextBlock textBlock = new TextBlock();
+                textBlock.Text = directory.Name;
+                textBlock.Margin = new Thickness(10, 5, 0, 0);
+
+                BitmapImage myBitmapImage = new BitmapImage();
+                myBitmapImage.BeginInit();
+                myBitmapImage.UriSource = new Uri("C:/Users/NC/source/repos/OS_FileExplorer/FileExplorer/resources/folder.png", UriKind.RelativeOrAbsolute);
+                myBitmapImage.DecodePixelWidth = 2048;
+                myBitmapImage.EndInit();
+                Image image = new Image();
+                image.Source = myBitmapImage;
+                image.Height = 25;
+                image.Width = 25;
+
+                dockPanel.Children.Add(image);
+                dockPanel.Children.Add(textBlock);
+                button.Content = dockPanel;
+                DirectoryTreeContain.Children.Add(button);
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            getDirectoryTree();
         }
     }
-    
+
 }
