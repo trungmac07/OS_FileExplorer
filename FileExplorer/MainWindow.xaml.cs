d7b0ba1b5061141941a22f5e27dd9e9678fc6dac
Trung <mttrung16@gmail.com>
2023-03-27 15:27:06 +0700

    merge voi 2 thang dau cu lan 2

--- FileExplorer/MainWindow.xaml.cs
+++ FileExplorer/MainWindow.xaml.cs
@@ -214,35 +214,24 @@ namespace FileExplorer
         {
             ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
             int count = 0;
-            foreach (ManagementObject wmi_HD in searcher.Get()) count++;
-            if (count > 0) count--;
+            foreach (ManagementObject wmi_HD in searcher.Get()) 
+                count++;
+            /*if (count > 0) 
+                count--;*/
             foreach (ManagementObject wmi_HD in searcher.Get())
             {
                 HardDrive hd = new HardDrive();
                 hd.Model = wmi_HD["Model"].ToString();
                 hd.Type = wmi_HD["InterfaceType"].ToString();
-                hdCollection.Insert(0, hd);
-                //Console.WriteLine(hd.Type.ToString());
+                Console.WriteLine(hd.Type);
+                if (hd.Type == "USB")
+                {
+                    createDiskButton(count, hd.Model, 1);
+                }
+                count--;
             }
-            searcher = new
-            ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMedia");
-
-            int i = 0;
-            foreach (ManagementObject wmi_HD in searcher.Get())
-            {
-                // get the hard drive from collection
-                // using index
-                HardDrive hd = (HardDrive)hdCollection[i];
-
-                // get the hardware serial no.
-                if (wmi_HD["SerialNumber"] == null)
-                    hd.SerialNo = "None";
-                else
-                    hd.SerialNo = wmi_HD["SerialNumber"].ToString();
-                ++i;
-            }
-
         }
+
 
         public void createDiskButton(int index, string s, int kind)
         {
@@ -292,7 +281,6 @@ namespace FileExplorer
 
             };
             DiskArea.Children.Add(DiskButton1);
-
 
         }
 
